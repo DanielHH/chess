cb2cfc55316a81aaf21517af04f3f5dfe203e515
Daniel Han Herzegh <danhe178@student.liu.se>
2016-04-18 11:07:25 +0200

    "Added menu-options. added enumclass for gamemodes"

--- Schack/src/main/ChessFrame.java
+++ Schack/src/main/ChessFrame.java
@@ -11,7 +11,7 @@ public class ChessFrame extends JFrame {
    private JFrame frame;
 
    public ChessFrame(Board board) {
-      super("Schackspel");
+      super("Schack");
       this.setLayout(new BorderLayout());
       createMenus();
 
@@ -25,18 +25,40 @@ public class ChessFrame extends JFrame {
 
    private void createMenus() {
       final JMenuBar menuBar = new JMenuBar();
+
       final JMenu options = new JMenu("Options");
+      final JMenuItem start = new JMenuItem("Start game", 'S');
+      final JMenuItem load = new JMenuItem("Load game", 'L');
+      final JMenuItem save = new JMenuItem("Save", 'S');
+      final JMenuItem reset = new JMenuItem("Reset", 'R');
       final JMenuItem quit = new JMenuItem("Quit", 'Q');
+
+      final JMenu mode = new JMenu("Mode");
+      final JMenuItem pvp = new JMenuItem("PvP");
+      final JMenuItem pvai = new JMenuItem("PvAI");
+      final JMenuItem aivai = new JMenuItem("AIvAI");
+      final JMenuItem editor = new JMenuItem("Editor");
+
       quit.addActionListener(new ExitListener());
+      options.add(start);
+      options.add(load);
+      options.add(save);
+      options.add(reset);
       options.add(quit);
       menuBar.add(options);
+
+      mode.add(pvp);
+      mode.add(pvai);
+      mode.add(aivai);
+      mode.add(editor);
+      menuBar.add(mode);
       this.setJMenuBar(menuBar);
    }
 
    private class ExitListener implements ActionListener
    {
       public void actionPerformed(ActionEvent e) {
-          int answer = JOptionPane.showConfirmDialog(frame, "Do you really wanna quit, n00b?", "Quit?",
+          int answer = JOptionPane.showConfirmDialog(frame, "Are you sure?", "Quit?",
                                                 JOptionPane.YES_NO_OPTION);
           if (answer == JOptionPane.YES_OPTION) {
              System.exit(0);
